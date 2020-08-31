a9db5e4cf6c616da94ddaf4d32e8169621890f43
kiootic <kiootic@gmail.com>
2020-08-31 23:26:49 +0800

    Fix blur filter rendering

--- packages/swf-lib/src/internal/render/filters/BlurFilter.ts
+++ packages/swf-lib/src/internal/render/filters/BlurFilter.ts
@@ -22,43 +22,35 @@ export class BlurFilter implements Filter {
     const radiusX = this.blurX / 2;
     const radiusY = this.blurY / 2;
     const quality = Math.ceil(Math.max(radiusX, radiusY) / 16) * 4;
-    const uDeltaX = vec2.fromValues(radiusX / 32 / ctx.width, 0);
-    const uDeltaY = vec2.fromValues(0, radiusY / 32 / ctx.height);
+    const uDeltaX = vec2.fromValues(radiusX / quality / ctx.width, 0);
+    const uDeltaY = vec2.fromValues(0, radiusY / quality / ctx.height);
 
     let from = ctx.target.textureAux1;
     let to = ctx.target.textureAux2;
     const blurProgram = programBlur(quality);
 
-    if (this.blurX > 0) {
-      for (let i = 0; i < this.passes; i++) {
-        ctx.applyFilter(
-          blurProgram,
-          { from: i === 0 ? ctx.target.texture : from, to },
-          (gl) => {
-            blurProgram.setUniform(gl, "uDelta", uDeltaX);
-          }
-        );
+    for (let i = 0; i < this.passes; i++) {
+      ctx.applyFilter(
+        blurProgram,
+        { from: i === 0 ? ctx.target.texture : from, to },
+        (gl) => {
+          blurProgram.setUniform(gl, "uDelta", uDeltaX);
+        }
+      );
 
-        const t = from;
-        from = to;
-        to = t;
-      }
+      const t = from;
+      from = to;
+      to = t;
     }
 
-    if (this.blurY > 0) {
-      for (let i = 0; i < this.passes; i++) {
-        ctx.applyFilter(
-          blurProgram,
-          { from: i === 0 ? ctx.target.texture : from, to },
-          (gl) => {
-            blurProgram.setUniform(gl, "uDelta", uDeltaY);
-          }
-        );
+    for (let i = 0; i < this.passes; i++) {
+      ctx.applyFilter(blurProgram, { from, to }, (gl) => {
+        blurProgram.setUniform(gl, "uDelta", uDeltaY);
+      });
 
-        const t = from;
-        from = to;
-        to = t;
-      }
+      const t = from;
+      from = to;
+      to = t;
     }
 
     ctx.blit(from, ctx.target.texture);
