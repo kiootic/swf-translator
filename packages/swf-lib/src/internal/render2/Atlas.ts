497a01a906b44d776542c2784e6225e26a78c487
kiootic <kiootic@gmail.com>
2020-09-17 01:03:21 +0800

    Fix atlas layout overflows vertically

--- packages/swf-lib/src/internal/render2/Atlas.ts
+++ packages/swf-lib/src/internal/render2/Atlas.ts
@@ -3,8 +3,8 @@ import { rect } from "../math/rect";
 const padding = 1;
 
 export class Atlas {
-  private x = padding;
-  private y = padding;
+  private x = 0;
+  private y = 0;
   private rowHeight = 0;
 
   constructor(readonly width: number, readonly height: number) {}
@@ -15,7 +15,7 @@ export class Atlas {
 
     if (this.x + width > this.width) {
       this.x = 0;
-      this.y += this.rowHeight;
+      this.y += this.rowHeight + padding;
       this.rowHeight = 0;
 
       x = this.x;
@@ -27,7 +27,7 @@ export class Atlas {
     }
     this.x += padding;
 
-    this.rowHeight = Math.max(this.rowHeight, height + padding);
+    this.rowHeight = Math.max(this.rowHeight, height);
     if (this.y + this.rowHeight > this.height) {
       return null;
     }
