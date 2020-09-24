f7c1237c05e73d307b78cabae4d616a984e9ca07
kiootic <kiootic@gmail.com>
2020-09-24 23:20:00 +0800

    Add more Rectangle API

--- packages/swf-lib/src/classes/flash/geom/Rectangle.ts
+++ packages/swf-lib/src/classes/flash/geom/Rectangle.ts
@@ -5,6 +5,34 @@ export class Rectangle {
 
   readonly __rect: rect;
 
+  get x() {
+    return this.__rect[0];
+  }
+  set x(value) {
+    this.__rect[0] = value;
+  }
+
+  get y() {
+    return this.__rect[1];
+  }
+  set y(value) {
+    this.__rect[1] = value;
+  }
+
+  get width() {
+    return this.__rect[2];
+  }
+  set width(value) {
+    this.__rect[2] = value;
+  }
+
+  get height() {
+    return this.__rect[3];
+  }
+  set height(value) {
+    this.__rect[3] = value;
+  }
+
   constructor(x = 0, y = 0, width = 0, height = 0) {
     this.__rect = rect.fromValues(x, y, width, height);
   }
