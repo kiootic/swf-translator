1adbd2327ce6ac76db5661980c7a4b704cc095c0
kiootic <kiootic@gmail.com>
2020-10-18 01:02:44 +0800

    Wrap canvas in container

--- packages/swf-lib/src/internal/render2/Canvas.ts
+++ packages/swf-lib/src/internal/render2/Canvas.ts
@@ -1,16 +1,24 @@
 import { vec2 } from "gl-matrix";
 
 export class Canvas {
+  readonly container = document.createElement("div");
   readonly element = document.createElement("canvas");
   #cursor = "";
 
   constructor(readonly width: number, readonly height: number) {
+    this.container.style.setProperty("width", `${width}px`);
+    this.container.style.setProperty("height", `${height}px`);
+    this.container.style.setProperty("position", "relative");
+
     this.element.tabIndex = 0;
     this.element.style.setProperty("outline", "none", "important");
+    this.element.style.setProperty("position", "absolute");
     this.element.style.setProperty("width", `${width}px`);
     this.element.style.setProperty("height", `${height}px`);
     this.element.width = width * window.devicePixelRatio;
     this.element.height = height * window.devicePixelRatio;
+
+    this.container.appendChild(this.element);
   }
 
   get cursor(): string {
